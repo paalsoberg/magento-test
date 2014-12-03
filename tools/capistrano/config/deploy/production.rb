f0d31d17e0c72c2689fc26346cbca118d669d5da
thinkcreateweb <paalsoberg@Macintosh.local>
2014-12-03 12:15:06 +0000

    added production config

--- tools/capistrano/config/deploy/production.rb
+++ tools/capistrano/config/deploy/production.rb
@@ -2,5 +2,5 @@ set :deploy_to, "/var/www/" + "domain"
 set :branch, "master"
 set :keep_releases, 5
 
-server "user@domain", :app, :primary => true
+server "root@178.62.126.214", :app, :primary => true
 # set :gateway, "user@host" # Use if you need to bounce through another server
