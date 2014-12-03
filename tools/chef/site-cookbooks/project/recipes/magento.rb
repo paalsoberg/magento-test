29dd683aa699c415cc2715981d2a5e10150074e7
paalsoberg <paalsoberg@Macintosh.local>
2014-12-03 16:22:11 +0000

    update

--- tools/chef/site-cookbooks/project/recipes/magento.rb
+++ tools/chef/site-cookbooks/project/recipes/magento.rb
@@ -1,9 +1,44 @@
+if node['ssl_certs_config']['encrypted']
+  encrypted_data_bag_merge "ssl_certs/#{node['ssl_certs_config']['data_bag_item']}"
+else
+  data_bag_merge "ssl_certs/#{node['ssl_certs_config']['data_bag_item']}"
+end
 
-if node['magento']['capistrano']['enabled'] == true
+::Chef::Recipe.send(:include, ConfigDrivenHelper::Util)
+
+node['nginx']['sites'].each_pair do |name, siteMash|
+  site = immutablemash_to_hash(siteMash)
+
+  if site['inherits']
+    site = ::Chef::Mixin::DeepMerge.hash_only_merge(
+      immutablemash_to_hash(node['nginx']['shared_config'][site['inherits']]),
+      site
+    )
+  end
+
+  site = ::Chef::Mixin::DeepMerge.hash_only_merge(immutablemash_to_hash(node["nginx-sites"]), site)
+
+  if site['type'] == 'magento'
+
+    cron_d "magento-#{name}" do
+      command "sh #{site['docroot']}/cron.sh"
+      user 'apache'
+    end
+
+    magento = immutablemash_to_hash(node['magento'])
+
+    if site['magento']
+      magento = ::Chef::Mixin::DeepMerge.hash_only_merge(
+        magento,
+        immutablemash_to_hash(site['magento']))
+    end
+
+    if site['capistrano']
       config_path = "#{site['capistrano']['deploy_to']}/shared/public"
     else
-      config_path = node['docroot']
+      config_path = site['docroot']
     end
+
 
 directory config_path do
   recursive true
@@ -17,4 +52,7 @@ template "#{config_path}/app/etc/local.xml" do
   variables({
     :magento => magento
   })
+end
+
+  end
 end
