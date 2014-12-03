6358ba7d3f9f5cdc596d5e027012ea53bf8c7b1d
paalsoberg <paalsoberg@Macintosh.local>
2014-12-03 15:40:15 +0000

    adding magento recipe

---
+++ tools/chef/site-cookbooks/project/recipes/magento.rb
@@ -0,0 +1,20 @@
+
+ if site['capistrano']
+      config_path = "#{site['capistrano']['deploy_to']}/shared/public"
+    else
+      config_path = site['docroot']
+    end
+
+directory config_path do
+  recursive true
+  mode "0755"
+  action :create
+end
+
+template "#{config_path}/app/etc/local.xml" do
+  source "local.xml.erb"
+  mode 0644
+  variables({
+    :magento => node['magento']
+  })
+end
